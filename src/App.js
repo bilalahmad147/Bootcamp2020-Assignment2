df9915c5c0bc90f17abb5f6477780ec8701bf3b6
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-06-02 12:28:12 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>This is first deployment by BILAL AHMAD</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
